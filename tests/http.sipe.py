46ef73c97e366dc1270c66e2d5f5e173c8241279
Hugo Leclerc <leclerc@lmt.ens-cachan.fr>
2012-03-08 09:18:43 +0100

    ...

--- tests/http.sipe.py
+++ tests/http.sipe.py
@@ -6,12 +6,15 @@ get_urls =
     # { beg_url = data + 1; } any** ' ' { coutn << "url='" << String( beg_url, data ) << "'"; }
 
 
-content_length = 'Content-Length: ' uint[ val = 'content_length' ] { std::cout << sipe_data->content_length << std::endl; }
+content_length =
+    'Content-Length: '
+    uint[ val = 'content_length' ]
+    { std::cout << sipe_data->content_length << std::endl; }
 
 post_data =
     (
         ( nl content_length ) |
-        #( new_line new_line @end_post_data ) |
+        #( cr cr @end_post_data ) |
         any
     )**
     #:end_post_data
@@ -21,28 +24,14 @@ put  = 'PUT ' { std::cout << "-> put\n"; } #{{ P( "put" ); }
 post = 'POST ' { std::cout << "-> post\n"; } post_data
 e400 = { std::cout << "ERROR 400: bad request type\n"; }
 
-p_data =
-    (
-        ( 'C' uint[ val = 'content_length' ] {P(sipe_data->content_length);} ) |
-        # {P("C");}
-        #( new_line new_line @end_post_data ) |
-        any
-    )**
-    #:end_post_data
-
-pot = 'P' p_data
-
 main =
     _add_prel[ '#define P(A) std::cout << #A << " -> " << A << std::endl;' ]
-    #'C' uint[ val = 'content_length' ] {P(sipe_data->content_length);}
-    pot
+    _set_strn[ 'Http' ]
 
-    #_set_strn[ 'Http' ]
-    #get  [freq=10] |
-    #put  [freq= 1] |
-    #post [freq= 6] |
-    #e400
-    #get | post | put | e400
+    get  [freq=10] |
+    put  [freq= 1] |
+    post [freq= 6] |
+    e400
     
 #main = uint[ val="toto" ] { std::cout << sipe_data->toto; }
 
