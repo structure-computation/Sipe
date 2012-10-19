12070ecda86a0860d55281ca74a216fd5ba9d0ff
Hugo Leclerc <leclerc@lmt.ens-cachan.fr>
2012-10-19 13:17:50 +0200

    License

--- src/Sipe/Predef.cpp
+++ src/Sipe/Predef.cpp
@@ -1,3 +1,23 @@
+/*
+ Copyright 2012 Structure Computation  www.structure-computation.com
+ Copyright 2012 Hugo Leclerc
+
+ This file is part of Sipe.
+
+ Sipe is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Sipe is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with Sipe. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 char predef[] = {
     35, 32, 99, 104, 97, 114, 32, 115, 101, 116, 115, 10, 97, 110, 121, 32, 
     32, 32, 32, 32, 32, 32, 61, 32, 48, 32, 46, 46, 32, 50, 53, 54, 
